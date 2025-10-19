3aa7f2a76faf1d0ae4c501d7044be9917dbd7939
tulonbaar <lukaszbar.bar@gmail.com>
2025-10-19 06:14:37 +0200

    P: Revert K
    This reverts commit d2af531631ce4cbaa47f7787c99ce379f5a74d49.

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,3 +1,1 @@
-printf "\n====== SCANNING FOR CREDIT CARD NUMBERS ======\n"
-grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "========= CREDIT CARD SCAN COMPLETE =========="
+# TODO: write the script
